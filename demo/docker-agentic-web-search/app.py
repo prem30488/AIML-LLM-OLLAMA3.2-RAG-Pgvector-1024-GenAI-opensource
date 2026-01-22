28b90e0132c2e67f2883742581e0ed8b9c318886
Parth Trivedi <prem30488@gmail.com>
2026-01-23 00:10:10 +0530

    updated news based stock prediction with considering LSTM for last 60 days data
    updated news based stock prediction with considering LSTM for last 60 days data, also with RSI and MACD

--- demo/docker-agentic-web-search/app.py
+++ demo/docker-agentic-web-search/app.py
@@ -14,37 +14,36 @@ from duckduckgo_search import DDGS
 
 import yfinance as yf
 from datetime import datetime, timedelta
+import numpy as np
+import pandas as pd
 
-
-OLLAMA_HOST = "http://host.docker.internal:11434"  # change to localhost if not docker
-NEWS_DOMAINS = (
-	"www.msn.com",
-    "thehindu.com",
-    "indianexpress.com",
-    "hindustantimes.com",
-    "timesofindia.indiatimes.com",
-    "livemint.com",
-    "economictimes.indiatimes.com",
-    "business-standard.com",
-    "moneycontrol.com",
-    "financialexpress.com",
-    "ndtv.com",
-    "news18.com",
-    "scroll.in",
-    "thewire.in",
-    "reuters.com",
-    "bbc.com",
-    "bloomberg.com",
-)
+from sklearn.preprocessing import MinMaxScaler
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import LSTM, Dense
+from tensorflow.keras.layers import Dropout
+from tensorflow.keras.optimizers import Adam
+OLLAMA_HOST = "http://host.docker.internal:11434"
 ALERT_RULES = {
     "sentiment_threshold": 0.6,
-    "price_move_pct": 0.01,     # % move
+    "price_move_pct": 0.01,
     "time_window_hours": 24
 }
 
-def domain_query(keyword: str) -> str:
-    domain_filters = " OR ".join(f"site:{d}" for d in NEWS_DOMAINS)
-    return f"{keyword} ({domain_filters})"
+def RSI(series, period=14):
+    delta = series.diff()
+    gain = delta.where(delta > 0, 0).rolling(period).mean()
+    loss = -delta.where(delta < 0, 0).rolling(period).mean()
+    rs = gain / loss
+    return 100 - (100 / (1 + rs))
+
+def MACD(series, fast=12, slow=26, signal=9):
+    ema_fast = series.ewm(span=fast).mean()
+    ema_slow = series.ewm(span=slow).mean()
+    macd = ema_fast - ema_slow
+    signal_line = macd.ewm(span=signal).mean()
+    return macd, signal_line
+
+
 
 def detect_sentiment_spike(topic):
     since = datetime.utcnow() - timedelta(hours=24)
@@ -73,6 +72,15 @@ def get_stock_prices(symbol: str, days: int = 2):
         return []
 
     return hist["Close"].tolist()
+    
+def get_stock_data(symbol: str, days: int = 2):
+    ticker = yf.Ticker(symbol)
+    hist = ticker.history(period=f"{days}d",interval="5m")
+
+    if hist.empty:
+        return []
+
+    return hist
 def percent_change(old_price: float, new_price: float) -> float:
     if old_price == 0:
         return 0.0
@@ -94,6 +102,79 @@ def detect_price_movement(symbol: str, threshold: float = 0.03):
 
     return percent_change(previous_close,latest_close)
 
+def predict_inference(topic, symbol):
+    price_move = 0.0
+    data = get_stock_data(symbol, days=60)
+    
+
+    data["RSI"] = RSI(data["Close"])
+    data["MACD"], data["MACD_SIGNAL"] = MACD(data["Close"])
+    data.dropna(inplace=True)
+
+    features = data[["Close", "RSI", "MACD", "MACD_SIGNAL"]].values
+    
+    feature_scaler = MinMaxScaler()
+    scaled = feature_scaler.fit_transform(features)
+
+    X, y = [], []
+    window = 30   # 30 candles = ~150 minutes
+
+    for i in range(window, len(scaled)):
+        X.append(scaled[i-window:i])
+        y.append(scaled[i, 0])   # predict Close price
+
+    X = np.array(X)
+    y = np.array(y)
+
+    #print("X shape:", X.shape)  # (samples, window, features)
+    split = int(0.8 * len(X))
+    X_train, X_test = X[:split], X[split:]
+    y_train, y_test = y[:split], y[split:]
+
+    model = Sequential([
+        LSTM(64, return_sequences=True, input_shape=(window, X.shape[2])),
+        Dropout(0.2),
+        LSTM(64),
+        Dropout(0.2),
+        Dense(1)
+    ])
+    optimizer = Adam(
+        learning_rate=5e-5,
+        clipnorm=1.0
+    )
+
+    model.compile(optimizer=optimizer, loss="mse")
+
+    #model.compile(optimizer="adam", loss="mse")
+    model.fit(X_train, y_train, epochs=8, batch_size=32)
+
+    predicted = model.predict(X_test)
+
+    # inverse scale CLOSE only
+    #close_scaler = MinMaxScaler()
+    feature_scaler.fit(data[["Close"]])
+
+    predicted_price = feature_scaler.inverse_transform(predicted)
+    actual_price = feature_scaler.inverse_transform(y_test.reshape(-1, 1))
+
+    last_close = actual_price[-1][0]
+    next_close = predicted_price[-1][0]
+    price_move = next_close
+    print(price_move)
+    change_pct = (next_close - last_close) / last_close
+    if change_pct > 0.003:
+        signal = "BUY 📈"
+    elif change_pct < -0.003:
+        signal = "SELL 📉"
+    else:
+        signal = "HOLD ⚖️"
+
+    return True, {
+		"signal": signal,
+        "price_move": change_pct,
+        "predicted_price":price_move
+    }
+        
 def should_alert(topic, symbol):
     sentiment = detect_sentiment_spike(topic)
     price_move = detect_price_movement(symbol)
@@ -129,10 +210,18 @@ def run_alerts():
     symbol = data["symbol"]
 
     trigger, meta = should_alert(topic, symbol)
-
+    price_from_news = meta['price_move']
+    triggerr, peta = predict_inference(topic,symbol)
+    price_from_price_sequence_predicton = peta['price_move']
+	
+    price = price_from_news + price_from_price_sequence_predicton
+    if price > 0:
+        price = price/2
     if not trigger:
         return jsonify({"status": "no alert"})
-
+    if not triggerr:
+        return jsonify({"status": "no alert"})
+    
     explanation = alert_explainer(meta)
 
     alert_msg = f"""
@@ -140,8 +229,11 @@ NEWS-DRIVEN STOCK ALERT
 Company: {topic}
 Symbol: {symbol}
 Sentiment: {meta['sentiment']:.2f}
-Price Move: {meta['price_move']:.2f}%
-
+Price Move: {price:.2f}%
+Signal: {peta['signal']}
+Predicted from News: {price_from_news:.2f}
+Predicted from Model: {price_from_price_sequence_predicton:.2f}
+Predicted price: {peta['predicted_price']:.2f} /- INR
 Reason:
 {explanation}
 """
@@ -267,27 +359,8 @@ def ingest():
     stored = 0
     for item in sample:
         if len(item["body"]) > 50:
-            store_document(item["body"],topic=keyword, published_at = datetime.fromisoformat(item["date"].replace("Z", "+00:00"))
-)
+            store_document(item["body"],topic=keyword, published_at = datetime.fromisoformat(item["date"].replace("Z", "+00:00")))
             stored += 1
-
-    #query = domain_query(keyword)
-
-    #result = search_agent.run(query)
-
-    #urls = [
-	#    line.strip()
-	#    for line in result.content.url
-	#    if line.startswith("https")
-	    #and any(domain in line for domain in NEWS_DOMAINS)
-    #]
-
-    #stored = 0
-    #for url in urls[:5]:
-    #    text = scrape_url(url)
-    #    if len(text) > 500:
-    #        store_document(text)
-    #        stored += 1
 
     return jsonify({
         "status": "ingested",
